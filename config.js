c25d5043d93ef1c7c2b96029f34a461e96f8fc3b
ysinghal22 <30543435+ysinghal22@users.noreply.github.com>
2018-12-05 01:00:47 +0530

    user_credentials
    generate user credentials from twitter developer account.

---
+++ config.js
@@ -0,0 +1,15 @@
+
+/* generate consumer_key,
+consumer_secret,
+access_token,
+access_token_secret
+of user account from twitter developer */
+
+//assign them below
+
+module.exports = {
+  consumer_key:         ,
+  consumer_secret:      ,
+  access_token:         ,
+  access_token_secret:  ,
+}
